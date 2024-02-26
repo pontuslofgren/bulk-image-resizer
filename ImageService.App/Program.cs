c51ccc3509af881776916334a42b0f515d4a348d
Pontus <pontus.loefgren@gmail.com>
2024-02-26 14:50:58 +0100

    Fixes possible null reference warnings.

--- ImageService.App/Program.cs
+++ ImageService.App/Program.cs
@@ -14,18 +14,18 @@ public class Program
         Console.WriteLine("Welcome to ImageResizer");
         Console.WriteLine("Please provide a path to a directory containing images: ");
 
-        DirectoryInfo inputDir;
+        DirectoryInfo? inputDir;
         do
         {
             Console.Write("> ");
-            inputDir = new DirectoryInfo(Console.ReadLine());
+            inputDir = new DirectoryInfo(Console.ReadLine()!);
         } while (!inputDir.Exists);
         
         Console.WriteLine("What would you like to do?");
         Console.WriteLine("Press [1] to resize image with a max pixel width");
         Console.WriteLine("Press [2] to resize image with a scale");
 
-        string userCommand;
+        string? userCommand;
         do
         {
             Console.Write("> ");
@@ -37,13 +37,13 @@ public class Program
             case "1":
                 Console.WriteLine("What is the max pixel width?");
                 Console.Write("> ");
-                int maxPixelWidth = int.Parse(Console.ReadLine());
+                int maxPixelWidth = int.Parse(Console.ReadLine()!);
                 ProcessAllImagesInDirectory(inputDir, "ResizeToMaxPixelWidth", maxPixelWidth);
                 break;
             case "2":
                 Console.WriteLine("What is the scale?");
                 Console.Write("> ");
-                int scale = (int) (double.Parse(Console.ReadLine()) * 100);
+                int scale = (int) (double.Parse(Console.ReadLine()!) * 100);
                 Console.WriteLine($"Scale: {scale}");
                 ProcessAllImagesInDirectory(inputDir, "ResizeWithScale", scale);
                 break;

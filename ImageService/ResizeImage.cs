9e8ed2e2e2a8bc0175aaeb17bb6ca6d82e2654f2
Pontus <pontus.loefgren@gmail.com>
2024-02-26 11:50:30 +0100

    Adds temp hard-coded code to resize image

--- ImageService/ResizeImage.cs
+++ ImageService/ResizeImage.cs
@@ -1,9 +1,20 @@
-﻿using SixLabors.ImageSharp;
+﻿using System.Reflection;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.Processing;
 
 namespace ImageService;
 
 public static class ResizeImage
 {
-    
+    private static readonly string _executionDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
+    private static readonly string _filePath = Path.Combine(_executionDirectory, "input");
+
+    public static void ResizeWithScale(int scale)
+    {
+        using Image image = Image.Load(Path.Combine(_filePath, "stockholm.jpeg"));
+        image.Mutate(x => x
+            .Resize(image.Width / 2, image.Height / 2));
+
+        image.Save(Path.Combine(_filePath, "output", "stockholm.jpeg"));
+    }
 }

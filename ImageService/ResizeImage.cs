9246419221a4f7ed47a2585f8edf6fb3d3e85b59
Pontus <pontus.loefgren@gmail.com>
2024-02-26 12:49:09 +0100

    Resize with scale can take double and increase/decrease size.

--- ImageService/ResizeImage.cs
+++ ImageService/ResizeImage.cs
@@ -6,11 +6,14 @@ namespace ImageService;
 
 public static class ResizeImage
 {
-    public static void ResizeWithScale(int scale)
+    public static void ResizeWithScale(double scale)
     {
         using Image image = Image.Load("input/group.jpg");
+
+        int newWidth = (int)(image.Width * scale);
+        
         image.Mutate(x => x
-            .Resize(image.Width / scale, image.Height / scale));
+            .Resize(newWidth, 0));
 
         image.Save("output/Stockholm_out.png");
     }

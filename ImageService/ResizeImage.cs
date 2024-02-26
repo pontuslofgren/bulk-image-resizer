111c4c97c47c35b1a95da4cfd32e6a3571825426
Pontus <pontus.loefgren@gmail.com>
2024-02-26 12:08:05 +0100

    Changes used directories. Program successfully scales down an image.

--- ImageService/ResizeImage.cs
+++ ImageService/ResizeImage.cs
@@ -6,15 +6,12 @@ namespace ImageService;
 
 public static class ResizeImage
 {
-    private static readonly string _executionDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location)!;
-    private static readonly string _filePath = Path.Combine(_executionDirectory, "input");
-
     public static void ResizeWithScale(int scale)
     {
-        using Image image = Image.Load(Path.Combine(_filePath, "stockholm.jpeg"));
+        using Image image = Image.Load("input/group.jpg");
         image.Mutate(x => x
-            .Resize(image.Width / 2, image.Height / 2));
+            .Resize(image.Width / scale, image.Height / scale));
 
-        image.Save(Path.Combine(_filePath, "output", "stockholm.jpeg"));
+        image.Save("output/Stockholm_out.png");
     }
 }

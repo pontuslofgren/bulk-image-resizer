4aaf7bc73f042ac6f4135b54a7ace6410c6d0e6b
Pontus <pontus.loefgren@gmail.com>
2024-02-26 14:19:30 +0100

    Resize methods get Fileinfo as parameter.

--- ImageService/ResizeImage.cs
+++ ImageService/ResizeImage.cs
@@ -6,15 +6,30 @@ namespace ImageService;
 
 public static class ResizeImage
 {
-    public static void ResizeWithScale(double scale)
+    public static void ResizeToMaxPixelWidth(int maxWidth, FileInfo file)
     {
-        using Image image = Image.Load("input/group.jpg");
+        Image image = Image.Load(file.FullName);
+        if (image.Width < maxWidth)
+        {
+            Console.WriteLine("Image width within bound.");
+            return;
+        }
+        
+        image.Mutate(x => x
+            .Resize(maxWidth, 0));
 
+        image.Save($"output/resized_{file.Name}");
+    }
+    
+    public static void ResizeWithScale(double scale, FileInfo file)
+    {
+        Image image = Image.Load(file.FullName);
         int newWidth = (int)(image.Width * scale);
         
         image.Mutate(x => x
             .Resize(newWidth, 0));
 
-        image.Save("output/Stockholm_out.png");
+        image.Save($"output/resized_{file.Name}");
     }
 }
+    
